700cd29e667b7084f69eb874b5ea9a3b95f230be
burak behlül <burakyabgu@gmail.com>
2025-09-14 12:14:05 +0300

    feat: Add developer settingto main

--- server/main.js
+++ server/main.js
@@ -1,2 +1,27 @@
+import express from 'express';
 
-console.log("hello e-commerce!")
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+
+import 'dotenv/config';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors({ origin: true, credentials: true }));
+app.use(morgan('dev'));
+app.use(helmet());
+
+
+app.get('/', (req, res) => {
+  res.send('hello e-commerce');
+});
+
+const PORT = process.env.PORT || 80;
+
+app.listen(PORT, () => {
+  console.log(`Sunucu ${PORT} portunda çalışıyor.`);
+});
+
