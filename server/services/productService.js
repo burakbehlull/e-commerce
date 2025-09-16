65a54ff470ca23f516225dc6b218263aaac651f4
burak behlül <burakyabgu@gmail.com>
2025-09-16 15:37:54 +0300

    feat: Add product service actions

--- server/services/productService.js
+++ server/services/productService.js
@@ -1,21 +1,120 @@
 import { Product } from "#models";
 
-function addProduct(data={name, description, price, 
-		category, brand, stock, thumbnail, isActive}){
-	if(!data) return { 
-		status: false,
-		message: "Veri Boş"
+async function addProduct(data){
+	try {
+		if(!data) return { 
+			status: false,
+			message: "Veri Boş"
+		}
+		
+		const createdProduct = await Product.create(data)
+		
+		return { 
+			status: true,
+			message: "Ürün oluşturuldu",
+			data: createdProduct,
+		}
+	} catch(err) {
+		console.error("[ERROR - productService/addProduct]: ", err.message)
+		return {
+			status: false,
+			error: err,
+			message: err.message
+		}
+	}
+}
+
+async function getProduct(id){
+	try {
+		if(!id) return { 
+			status: false,
+			message: "Eksik istek"
+		}
+		
+		const product = await Product.findById(id)
+		if(!product) return {
+			status: false,
+			message: "Ürün mevcut değil",
+		}
+		return {
+			status: true,
+			message: "Ürün bilgileri çekildi",
+			data: product
+		}
+	} catch(err) {
+		console.error("[ERROR - productService/getProduct]: ", err.message)
+		return {
+			status: false,
+			error: err,
+			message: err.message
+		}
 	}
 	
-	const createdProduct = await Product.create(data)
+}
+
+async function updateProduct(id, data){
+	try {
+		if(!id || !data) return { 
+			status: false,
+			message: "Eksik istek"
+		}
+		const product = await Product.findById(id)
+		if(!product) return {
+			status: false,
+			message: "Ürün mevcut değil",
+		}
+		
+		const updatedProduct = await Product.findByIdAndUpdate(id, { $set: data }, { new: true, runValidators: true })
+		return {
+			status: true,
+			message: "Ürün güncellendi",
+			data: updatedProduct
+		}
+	} catch(err) {
+		console.error("[ERROR - productService/updateProduct]: ", err.message)
+		return {
+			status: false,
+			error: err,
+			message: err.message
+		}
+	}
 	
-	return { 
-		status: true,
-		message: "Ürün oluşturuldu",
-		data: createdProduct,
+}
+
+async function deleteProduct(id){
+	try {
+		if(!id) return { 
+			status: false,
+			message: "Eksik istek"
+		}
+		
+		const product = await Product.findById(id)
+		
+		if(!product) return {
+			status: false,
+			message: "Ürün mevcut değil",
+		}
+		
+		const deletedProduct = await Product.findByIdAndDelete(id);
+		
+		return {
+			status: true,
+			message: "Ürün silindi!",
+			data: deletedProduct,
+		}
+	} catch(err) {
+		console.error("[ERROR - productService/deleteProduct]: ", err.message)
+		return {
+			status: false,
+			error: err,
+			message: err.message
+		}
 	}
 }
 
 export {
-	addProduct
+	addProduct,
+	getProduct,
+	updateProduct,
+	deleteProduct
 };
