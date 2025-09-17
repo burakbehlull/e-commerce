76fe8db48aeeac75aaeb5fad22065d9aac73c4ca
burak behlül <burakyabgu@gmail.com>
2025-09-17 13:41:41 +0300

    feat: Add get api function

--- server/helpers/misc.js
+++ server/helpers/misc.js
@@ -5,6 +5,20 @@ function generateId(val=11){
 	return id
 }
 
+function getIp(req){
+  // app.set("trust proxy", true);
+  const forw = req.ip
+  const forwardedFor = req.headers['x-forwarded-for'];
+  const originalIp = forwardedFor ? forwardedFor.split(',')[0] : forw;
+  
+  const ra = req.socket.remoteAddress
+  
+  const ip = originalIp ? originalIp : ra
+  
+  return { status: true, data: ip }
+}
+
 export {
-	generateId
+	generateId,
+	getIp
 }
