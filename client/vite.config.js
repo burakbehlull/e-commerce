2822da7d7509be855bf4e4d8ebe2804bc8d38eca
burak behlül <burakyabgu@gmail.com>
2025-09-19 23:36:36 +0300

    refactor (frontend): Change interface application settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import tsconfigPaths from "vite-tsconfig-paths"
+import * as path from "node:path"
+
+function way(name){
+  return path.resolve(__dirname, name)
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
-	  port: 8000
-  }
+    port: 8000,
+  },
+  resolve: {
+    alias: {
+      '~': way('src'),
+    },
+  },
 })
