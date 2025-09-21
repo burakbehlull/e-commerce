7c124b9b6259f6b3195088f639310b1b805c5be4
burak behlül <burakyabgu@gmail.com>
2025-09-21 15:51:34 +0300

    feat: Create api base file

---
+++ client/src/base/api.js
@@ -0,0 +1,61 @@
+import axios from "axios"
+
+const API = axios.create({
+    baseURL: 'http://localhost:80/api'
+});
+
+const apiRequest = {
+    get: async (url, params = {}, config = {}) => {
+        try {
+            const response = await API.get(url, { ...config, params });
+            return response.data;
+        } catch (error) {
+            console.error('GET Error:', error);
+            throw error;
+        }
+    },
+    post: async (url, data, config = {}) => {
+        try {
+            const response = await API.post(url, data, config);
+            return response.data;
+        } catch (error) {
+            console.error('POST Error:', error);
+            throw error;
+        }
+    },
+    put: async (url, data, config = {}) => {
+        try {
+            const response = await API.put(url, data, config);
+            return response.data;
+        } catch (error) {
+            console.error('PUT Error:', error);
+            throw error;
+        }
+    },
+    patch: async (url, data, config = {}) => {
+        try {
+            const response = await API.patch(url, data, config);
+            return response.data;
+        } catch (error) {
+            console.error('PATCH Error:', error);
+            throw error;
+        }
+    },
+    delete: async (url, config = {}, data) => {
+        try {
+            const response = await API.delete(url, {
+                data,
+                ...config
+            });
+            return response.data;
+        } catch (error) {
+            console.error('DELETE Error:', error);
+            throw error;
+        }
+    }
+};
+
+export {
+    API as BASEAPI,
+	apiRequest as API	
+}
--- client/vite.config.js
+++ client/vite.config.js
@@ -16,6 +16,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '~': way('src'),
+      '@api': way('src/base/api.js'),
     },
   },
 })
