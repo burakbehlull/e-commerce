cb2f39f558089b93c42107f49aa4b60e9338d988
burak behlül <burakyabgu@gmail.com>
2025-09-27 13:41:34 +0300

    feat: Add mail sender class

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -1,3 +1,4 @@
 export * from "./encryption.js"
 export * from "./misc.js"
+export mailSender from "./mailSender.js"
 
---
+++ server/helpers/mailSender.js
@@ -0,0 +1,44 @@
+import nodemailer from 'nodemailer'
+import 'dotenv/config'
+
+export default class mailSender {
+	constructor(){
+		this.transporter = nodemailer.createTransport({
+			service: 'gmail',
+			auth: {
+				user: process.env.GMAIL_USER,
+				pass: process.env.GMAIL_APP_PASSWORD
+			}
+		})
+		
+	}
+	send(user, { text, html, title }){
+		const mailOptions = {
+			from: process.env.GMAIL_USER,
+			to: user,
+			subject: title,
+			text,
+			html
+		}
+		
+		this.transporter.sendMail(mailOptions, (err, data)=> {
+			if(err) {
+				console.error("[Error / mailSender]: ", err)
+				return { 
+					status: false, 
+					message: 'Mail gönderimi Başarısız',
+					error: err
+				}
+			
+			} else {
+				// ..
+				return {
+					status: true, 
+					message: 'Mail gönderildi.',
+					data: data
+				}
+			}
+		})
+	}
+}
+
